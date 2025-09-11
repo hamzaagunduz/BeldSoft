e644b8327bf87d955839b6610492eabfb85de6f2
Hamza <hamza.gunduuz@gmail.com>
2025-09-11 03:03:54 +0300

    UserTemplate_Added_ComponentViewsUpdated

---
+++ Presentation/Beldsoft.MVC/ViewComponents/HomeComponents/HeroComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beldsoft.MVC.ViewComponents.HomeComponents
+{
+    public class HeroComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); // Default.cshtml çağrılır
+        }
+    }
+}
---
+++ Presentation/Beldsoft.MVC/ViewComponents/LayoutViewComponentsFront/FrontScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Beldsoft.MVC.ViewComponents.LayoutViewComponentsFront
+{
+    public class FrontScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); // Default.cshtml çağrılır
+        }
+    }
+}
